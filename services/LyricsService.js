26a0e3c853768d8d6fa7250b3ab99d15a784965d
Jason Yu <me@ycmjason.com>
2018-01-24 22:30:58 +0000

    TODO write test for LyricsService

--- services/LyricsService.js
+++ services/LyricsService.js
@@ -1,4 +1,4 @@
-const fetch = require('node-fetch');
+const fetch = require('node-fetch').default;
 const cheerio = require('cheerio');
 
 class LyricsService {
---
+++ test/services/LyricsService.test.js
@@ -0,0 +1,12 @@
+const assert = require('assert');
+
+const fetch = require('node-fetch');
+const cheerio = require('cheerio');
+
+describe('LyricsService', function() {
+  describe('fetch()', function() {
+    it('# should call fetch', function() {
+                  
+    });
+  });
+});
