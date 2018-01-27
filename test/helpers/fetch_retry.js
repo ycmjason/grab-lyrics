bff6983ec5f0361c5a6b28153834c88a15841f9f
Jason Yu <me@ycmjason.com>
2018-01-27 00:02:46 +0000

    added host header

--- test/helpers/fetch_retry.js
+++ test/helpers/fetch_retry.js
@@ -4,6 +4,7 @@ const DEFAULT_OPTIONS = {
   method: 'GET',
   headers: {
     'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.108 Safari/537.36',
+    'Host': '111.222.159.30',
   },
 };
 
